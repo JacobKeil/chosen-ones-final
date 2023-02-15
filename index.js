453426aa51ea2d2fb87be13c9ff0ee50158251c1
Jacob Keil <jacobkeil13@gmail.com>
2023-02-15 03:58:08 -0500

    rollback

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { handler } from "./client/handler.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
